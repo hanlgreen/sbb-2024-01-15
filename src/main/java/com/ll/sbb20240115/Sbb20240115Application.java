b871c5e6616a885cdef273c165ce818ea0fd0cdb
Park_Geyonggu <luckygoldsta@naver.com>
2024-01-15 10:58:41 +0900

    mission

---
+++ src/main/java/com/ll/sbb20240115/Sbb20240115Application.java
@@ -0,0 +1,23 @@
+package com.ll.sbb20240115;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/batch")
+@RequiredArgsConstructor
+public class BatchController {
+    private final BatchService batchService;
+
+    @GetMapping("/hello")
+    @ResponseBody
+    public String runSimpleJob(){
+        batchService.runHelloJob();
+
+        return "HelloJob OK";
+    }
+}
---
+++ src/test/java/com/ll/sbb20240115/Sbb20240115ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.sbb20240115;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sbb20240115ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
